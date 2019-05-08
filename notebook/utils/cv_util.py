21630536ba692229056d7e9e47115aa8ff7bdd3b
鈴木康元 <n8anko10mamenoki@gmail.com>
2019-05-09 00:57:53 +0900

    04_PSM追加

--- notebook/utils/cv_util.py
+++ notebook/utils/cv_util.py
@@ -2,13 +2,28 @@ import cv2
 import matplotlib.pyplot as plt
 
 # RGBを正しい値に変換して画像を読み込む
-def read_image (path:str):
-    im_cv = cv2.imread(test_path, 1)
-    img_src = cv2.cvtColor(im_cv, cv2.COLOR_BGR2RGB)
+def read_image_rgb (path:str):
+    img_src = cv2.imread(path, 1)
+    img_src = cv2.cvtColor(img_src, cv2.COLOR_BGR2RGB)
     return img_src
 
 # 画像を表示
 def show_color(obj, figsize=(14, 11)):
     fig, ax = plt.subplots(figsize=figsize)
     plt.imshow(obj)  #貼り付け
+    plt.tick_params(labelbottom=False, labelleft=False, labelright=False, labeltop=False) # ラベルを削除する
+    plt.tick_params(bottom=False, left=False, right=False, top=False) # 目盛りを削除する
     plt.show()
+
+# tesseract_ocrの返すbox情報をimageに書き込む    
+def draw_tesseract_boxes(img_temp, tesseract_boxes, color=(255,0,0)):
+    for box_index in range(len(tesseract_boxes)):
+        tesseract_box = tesseract_boxes[box_index]
+        left = tesseract_box.position[0][0]
+        top = tesseract_box.position[0][1]
+        right = tesseract_box.position[1][0]
+        bottom = tesseract_box.position[1][1]
+        
+        if left > 0 and top > 0: ## left, top が両方ゼロで不正な値が入ることがあるため除外する
+            cv2.rectangle(img_temp, (left, top), (right,bottom), color, 2)
+    return img_temp    
--- notebook/utils/text_util.py
+++ notebook/utils/text_util.py
@@ -1,3 +1,4 @@
+import difflib
 
 # textデータ読み込み
 def read_text (path:str) -> str:
@@ -5,3 +6,38 @@ def read_text (path:str) -> str:
     with open(path) as f:
         y = f.read()
     return y
+
+# 一行分のスコア算出
+def calc_line_ratio (line1:str, text2:[str]) -> float:
+    max_score = -1
+    for line2 in text2:
+        max_score = max(max_score, difflib.SequenceMatcher(None, line1, line2).ratio())
+    
+    max_score *= len(line1)
+#     print(f"{line1} max_score={max_score}")  # 行ごとのスコアが見たい時はコメントアウトを外して下さい
+    return max_score
+
+# 前処理
+def preprocessing_text (text1:str) -> [str]:
+    result = []
+    text1_array = text1.split("\n") # 改行で区切る
+    for line1 in text1_array:
+        line1 = line1.replace(' ', '') # スペースは削除
+        if len(line1) < 1:  # 空文字列の場合は何もしない。
+            continue
+        result.append(line1)
+    return result
+
+# 複数行計算（独自実装）
+def calc_text_ratio (text1:str, text2:str) -> float:
+    text1_array = preprocessing_text(text1)
+    text2_array = preprocessing_text(text2)
+    sum_score = 0
+    sum_length = 0
+    for line1 in text1_array:
+        sum_score += calc_line_ratio(line1, text2_array)
+        sum_length += len(line1)
+        
+    if sum_length == 0:
+        return 0.0
+    return sum_score/sum_length
